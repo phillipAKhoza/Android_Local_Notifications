442e74440f15c9321e208fc235f95500b75ea589
phillip khoza <phillipantonk@gmail.com>
2024-02-23 16:34:00 +0200

    declare button var

--- app/src/main/java/com/phillip_dev/local_notification/MainActivity.kt
+++ app/src/main/java/com/phillip_dev/local_notification/MainActivity.kt
@@ -2,12 +2,14 @@ package com.phillip_dev.local_notification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.phillip_dev.local_notification.databinding.ActivityMainBinding
 import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var appBinding: ActivityMainBinding
+    lateinit var count: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         appBinding = ActivityMainBinding.inflate(layoutInflater)
