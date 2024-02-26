addedb367580df6d286843ba7438c6f7a2ce0cd6
phillip khoza <phillipantonk@gmail.com>
2024-02-26 11:49:18 +0200

    implement broadcast receiver class

--- app/src/main/java/com/phillip_dev/local_notification/NotificationReceiver.kt
+++ app/src/main/java/com/phillip_dev/local_notification/NotificationReceiver.kt
@@ -1,6 +1,7 @@
 package com.phillip_dev.local_notification
 
 import android.Manifest
+import android.app.Activity
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.BroadcastReceiver
@@ -11,16 +12,17 @@ import android.os.Build
 import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.getSystemService
 
 class NotificationReceiver : BroadcastReceiver() {
     private  val CHANNEL_ID = "1"
     override fun onReceive(context: Context?, intent: Intent?) {
         if(context !=null){
-            val builder = NotificationCompat.Builder(this@MainActivity,CHANNEL_ID)
+            val builder = NotificationCompat.Builder(context,CHANNEL_ID)
             if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
 
                 val chanel = NotificationChannel(CHANNEL_ID,"1", NotificationManager.IMPORTANCE_DEFAULT)
-                val manager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                val manager : NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
                 manager.createNotificationChannel(chanel)
 
                 builder.setSmallIcon(R.drawable.notification_icon)
@@ -33,10 +35,10 @@ class NotificationReceiver : BroadcastReceiver() {
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
             }
-            val notificationManagerCompat = NotificationManagerCompat.from(this@MainActivity)
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED
+            val notificationManagerCompat = NotificationManagerCompat.from(context)
+            if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED
             ) {
-                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.POST_NOTIFICATIONS),1)
+                ActivityCompat.requestPermissions(context as Activity, arrayOf(Manifest.permission.POST_NOTIFICATIONS),1)
             }else {
                 notificationManagerCompat.notify(1, builder.build())
             }
