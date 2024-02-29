7824ac89dd1d9426644c431c169436049a8755d1
phillip khoza <phillipantonk@gmail.com>
2024-02-29 21:52:48 +0200

    close notification

--- app/src/main/java/com/phillip_dev/local_notification/CancelReceiver.kt
+++ app/src/main/java/com/phillip_dev/local_notification/CancelReceiver.kt
@@ -3,9 +3,12 @@ package com.phillip_dev.local_notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.core.app.NotificationManagerCompat
 
 class CancelReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
-
+        context?.let {
+            val notificationManagerCompat = NotificationManagerCompat.from(it)
+        }
     }
 }
