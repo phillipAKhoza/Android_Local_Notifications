7da1d91e19dce1ecda85deefb162ecd37e3312e7
phillip khoza <phillipantonk@gmail.com>
2024-02-29 21:33:29 +0200

    pending action button intent get broadcats

--- app/src/main/java/com/phillip_dev/local_notification/MainActivity.kt
+++ app/src/main/java/com/phillip_dev/local_notification/MainActivity.kt
@@ -56,7 +56,7 @@ class MainActivity : AppCompatActivity() {
         val pendingActionIntent = if(Build.VERSION.SDK_INT >=23){
             PendingIntent.getBroadcast(applicationContext,3,actionIntent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
         }else{
-            PendingIntent.getActivity(applicationContext,3,actionIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+            PendingIntent.getBroadcast(applicationContext,3,actionIntent, PendingIntent.FLAG_UPDATE_CURRENT)
 
         }
 
