e4f38261cdce218ed242a9f8dbb87510d0b611d5
phillip khoza <phillipantonk@gmail.com>
2024-02-29 21:35:07 +0200

    add ActionReceiver for action intent

---
+++ app/src/main/java/com/phillip_dev/local_notification/ActionReceiver.kt
@@ -0,0 +1,11 @@
+package com.phillip_dev.local_notification
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class ActionReceiver :BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+}
