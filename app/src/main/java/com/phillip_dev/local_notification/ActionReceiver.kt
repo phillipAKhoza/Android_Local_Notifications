c3e10bab3d3722f1149fbc05d5cec57ead6fdff0
phillip khoza <phillipantonk@gmail.com>
2024-02-29 21:38:00 +0200

    show toast upon action button press

--- app/src/main/java/com/phillip_dev/local_notification/ActionReceiver.kt
+++ app/src/main/java/com/phillip_dev/local_notification/ActionReceiver.kt
@@ -3,9 +3,12 @@ package com.phillip_dev.local_notification
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class ActionReceiver :BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
+        val toastMessage = intent?.getStringExtra("toast")
 
+        Toast.makeText(context,toastMessage,Toast.LENGTH_LONG).show()
     }
 }
